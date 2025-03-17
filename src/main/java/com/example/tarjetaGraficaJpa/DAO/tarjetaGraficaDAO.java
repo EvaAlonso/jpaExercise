aba9abbdd6201141e9fcc6d72d33ba86afb0460f
lltosso <luis.tosso@gmail.com>
2025-03-17 13:12:43 +0100

    adding in tarjetaGraficaDAO

--- src/main/java/com/example/tarjetaGraficaJpa/DAO/tarjetaGraficaDAO.java
+++ src/main/java/com/example/tarjetaGraficaJpa/DAO/tarjetaGraficaDAO.java
@@ -3,5 +3,39 @@ package com.example.tarjetaGraficaJpa.DAO;
 public interface tarjetaGraficaDAO {
 
     void save(tarjetaGrafica tarjetaGrafica);
+
     tarjetaGrafica findById(Integer id);
+
+    List<TarjetaGrafica> findAll();
+
+}
+@Override
+public List<TarjetaGrafica> findAll() {
+  TypedQuery<TarjetaGrafica> theQuery;
+  theQuery =
+ entityManager.createQuery("FROM TarjetaGrafica", TarjetaGrafica.class);
+  return theQuery.getResultList();
+}
+@Repository
+public class TarjetaGrafica implements tarjetaGraficaDAO{
+
+    @Override
+ public TarjetaGrafica findById(Integer id) {
+return this.entityManager.find(TarjetaGrafica.class, id);
+ }
+    @Override
+ @Transactional
+public void update(TarjetaGrafica tarjetaGrafica) {
+ entityManager.merge(tarjetaGrafica);
+}
+    @Override
+@Transactional
+public void delete(Integer id) {
+
+TarjetaGrafica tarjetaGrafica = this.entityManager.find(TarjetaGrafica.class, id);
+ entityManager.remove(tarjetaGrafica);
+}
+
+
+
 }
