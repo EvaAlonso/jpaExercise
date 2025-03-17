33e213317674f5653f85ca2c227abfa48e73b5d2
lltosso <luis.tosso@gmail.com>
2025-03-17 14:31:55 +0100

    Scanner in TarjetagraficaJPAplication added

--- src/main/java/com/example/tarjetaGraficaJpa/TarjetaGraficaJpaApplication.java
+++ src/main/java/com/example/tarjetaGraficaJpa/TarjetaGraficaJpaApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class TarjetaGraficaJpaApplication {
 
@@ -13,11 +15,27 @@ public class TarjetaGraficaJpaApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(String[] args) {
 		return runner -> {
 			System.out.println("Hola");
 		};
+		//crear objeto Scanner para leer
+		Scanner scanner = new Scanner(System.in);
+
+
+			while (true) {
+				System.out.println("Seleccionar opcion:");
+				System.out.println("1. Guardar una tarjeta grafica");
+				System.out.println("2. Buscar tarjeta grafica por ID");
+				System.out.println("3. Mostrar todas las tarjetas graficas");
+				System.out.println("4. Actualizar una tarjeta grafica");
+				System.out.println("5. Eliminar una tarjeta grafica");
+				System.out.println("6. Salir");
+
+				int opcion = scanner.nextInt();
+			}
+		}
 	}
-}
+
 
 
