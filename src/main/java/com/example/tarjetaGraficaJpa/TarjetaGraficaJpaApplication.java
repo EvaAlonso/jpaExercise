32ff2a91953172a4cc4f40a3481476beee9cc815
Eva Alonso Contreras <Eva.Alonso@proton.me>
2025-03-17 13:05:54 +0100

    add entity tarjetaGrafica

--- src/main/java/com/example/tarjetaGraficaJpa/TarjetaGraficaJpaApplication.java
+++ src/main/java/com/example/tarjetaGraficaJpa/TarjetaGraficaJpaApplication.java
@@ -1,7 +1,9 @@
 package com.example.tarjetaGraficaJpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TarjetaGraficaJpaApplication {
@@ -10,4 +12,12 @@ public class TarjetaGraficaJpaApplication {
 		SpringApplication.run(TarjetaGraficaJpaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner -> {
+			System.out.println("Hola");
+		};
+	}
 }
+
+
