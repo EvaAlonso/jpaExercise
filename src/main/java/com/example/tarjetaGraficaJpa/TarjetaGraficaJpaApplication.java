8edad5dd42e4a96c3d6a7e4e16198c83a0352c77
Eva Alonso Contreras <Eva.Alonso@proton.me>
2025-03-17 12:37:45 +0100

    add dependency

---
+++ src/main/java/com/example/tarjetaGraficaJpa/TarjetaGraficaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarjetaGraficaJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarjetaGraficaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarjetaGraficaJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tarjetaGraficaJpa/TarjetaGraficaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarjetaGraficaJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarjetaGraficaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
