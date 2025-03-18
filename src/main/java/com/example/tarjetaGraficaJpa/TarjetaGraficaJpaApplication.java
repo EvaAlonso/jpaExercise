4b6aac2b0177d88a66563d6ce786b0759d7fdf63
lltosso <luis.tosso@gmail.com>
2025-03-18 14:31:11 +0100

    modificado en Application para que muestre todos datos en pantalla

--- src/main/java/com/example/tarjetaGraficaJpa/TarjetaGraficaJpaApplication.java
+++ src/main/java/com/example/tarjetaGraficaJpa/TarjetaGraficaJpaApplication.java
@@ -25,41 +25,39 @@ public class TarjetaGraficaJpaApplication {
 			//System.out.println("Estudiante encontrado: " + tarjetaGrafica.getId());
 			for (TarjetaGrafica tarjetaGrafica1 : tarjetaGraficaDAO.findAll()) {
 				System.out.println(tarjetaGrafica1);
-
-				//crear objeto Scanner para leer
-				Scanner scanner = new Scanner(System.in);
-
-				while (true) {
-					System.out.println("Seleccionar opcion:");
-					System.out.println("1. Guardar una tarjeta grafica");
-					System.out.println("2. Buscar tarjeta grafica por ID");
-					System.out.println("3. Mostrar todas las tarjetas graficas");
-					System.out.println("4. Actualizar una tarjeta grafica");
-					System.out.println("5. Eliminar una tarjeta grafica");
-					System.out.println("6. Salir");
-
-					int opcion = scanner.nextInt();
-
-					if (opcion == 1) {
-						System.out.println("Guardando una tarjeta grafica....");
-					} else if (opcion == 2) {
-						System.out.println("Buscando una tarjeta grafica por ID...");
-					} else if (opcion == 3) {
-						System.out.println("Mostrando una tarjeta grafica....");
-					} else if (opcion == 4) {
-						System.out.println("Actualizando una tarjeta grafica....");
-					} else if (opcion == 5) {
-						System.out.println("Eliminando una tarjeta grafica");
-					} else if (opcion == 6) {
-						System.out.println("Saliendo del programa....");
-						scanner.close();
-						break;
-					} else {
-						System.out.println("Opción no valida. Intentelo de nuevo.");
-					}
-				}
 			}
-			;
+			//crear objeto Scanner para leer
+			Scanner scanner = new Scanner(System.in);
+
+			while (true) {
+				System.out.println("Seleccionar opcion:");
+				System.out.println("1. Guardar una tarjeta grafica");
+				System.out.println("2. Buscar tarjeta grafica por ID");
+				System.out.println("3. Mostrar todas las tarjetas graficas");
+				System.out.println("4. Actualizar una tarjeta grafica");
+				System.out.println("5. Eliminar una tarjeta grafica");
+				System.out.println("6. Salir");
+
+				int opcion = scanner.nextInt();
+
+				if (opcion == 1) {
+					System.out.println("Guardando una tarjeta grafica....");
+				} else if (opcion == 2) {
+					System.out.println("Buscando una tarjeta grafica por ID...");
+				} else if (opcion == 3) {
+					System.out.println("Mostrando una tarjeta grafica....");
+				} else if (opcion == 4) {
+					System.out.println("Actualizando una tarjeta grafica....");
+				} else if (opcion == 5) {
+					System.out.println("Eliminando una tarjeta grafica");
+				} else if (opcion == 6) {
+					System.out.println("Saliendo del programa....");
+					scanner.close();
+					break;
+				} else {
+					System.out.println("Opción no valida. Intentelo de nuevo.");
+				}
+			};
 		};
 	}
 }
